ada993d8ad1f0ecba8c8f9f104dcbe3368f4ac5a
Alex Fridman <AlexFridman@outlook.com>
2015-10-24 15:35:19 +0300

    Remove dummy test of cached logic

--- LabWork2/LabWork2/tests/cached_decorator/TestCachedBehavior.py
+++ LabWork2/LabWork2/tests/cached_decorator/TestCachedBehavior.py
@@ -1,21 +1,28 @@
 __author__ = 'AlexF'
 
 import unittest
+
 from LabWork2.code.cached_decorator.cached import cached
 
 
 class TestCachedBehavior(unittest.TestCase):
-    def test_caching(self):
-        @cached
-        def simple_function(x: tuple):
-            return x
+    def test_caching_with_class_instance(self):
+        class IntWrapper:
+            def __init__(self, value):
+                self.value = value
 
-        arg = ('test',)
+        @cached
+        def simple_function(x: IntWrapper):
+            return x.value + 1
+
+        arg = IntWrapper(5)
         result_1 = simple_function(arg)
+        arg.value += 1
         result_2 = simple_function(arg)
 
-        self.assertIs(arg, result_1)
-        self.assertIs(arg, result_2)
+        self.assertIs(6, result_1)
+        self.assertIs(6, result_2)
+
 
 if __name__ == '__main__':
     unittest.main()
