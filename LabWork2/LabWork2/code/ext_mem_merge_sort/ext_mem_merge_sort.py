135edc8eac80b216fd769b4715c7f3a16e0f118b
Alex Fridman <AlexFridman@outlook.com>
2015-10-27 02:04:25 +0300

    Add recursive-based ext_mem_merge_sort_impl

--- LabWork2/LabWork2/code/ext_mem_merge_sort/ext_mem_merge_sort.py
+++ LabWork2/LabWork2/code/ext_mem_merge_sort/ext_mem_merge_sort.py
@@ -1,27 +1,21 @@
 __author__ = 'AlexF'
 
 import os
-import math
 import tempfile
-from heapq import heappush, heappop, heapify
-from LabWork2.code.ext_mem_merge_sort.invalid_chunk_borders_error import InvalidChunkBordersError
-from LabWork2.code.ext_mem_merge_sort.invalid_data_error import InvalidDataError
-from LabWork2.code.ext_mem_merge_sort.cannot_create_temp_file_error import CannotCreateTempFileError
+
+import numpy as np
 
 
 class ExternalMemoryMergeSort:
     def sort(self, path, result_path, verbose=False):
         if not self._is_file_exists(path):
             raise FileNotFoundError('file {} not exists'.format(path))
+        self._input_path = path
         self._data_size = self._define_data_size(path)
-        self._chunk_count, self._chunk_shifts = self._define_chunk_count_and_shifts(self._data_size)
-        self._temp_files = {}
-        for chunk_i in range(self._chunk_count):
-            if verbose:
-                print('#{}'.format(chunk_i))
-            self._sort_chunk(path, chunk_i)
-
-        self.merge(result_path)
+        self._max_chunk_size = self._define_max_chunk_size()
+        self._verbose = verbose
+        temp_res = self._sort_data(0, self._data_size)
+        self._copy_from_temp(temp_res, result_path)
 
     def _is_file_exists(self, path):
         return os.path.exists(path)
@@ -30,103 +24,92 @@ class ExternalMemoryMergeSort:
         with open(path, 'r') as fp:
             return sum(1 for line in fp)
 
-    def _define_chunk_count_and_shifts(self, data_size):
-        chunk_size = self._define_max_chunk_size()
-        chunk_count = math.ceil(data_size / chunk_size)
-        chunk_shifts = dict([(chunk_n, chunk_n * chunk_size)
-                             for chunk_n in range(chunk_count)])
-        return chunk_count, chunk_shifts
-
     def _define_max_chunk_size(self):
-        return 10000
+        return 10 ** 5
 
-    def _sort_chunk(self, path, chunk_n):
-        data = self._read_chunk_from_init_file(path, chunk_n)
-        sorted_data = self._merge_sort(data)
-        temp_file = self._create_temp_file()
-        if not temp_file:
-            raise CannotCreateTempFileError()
-        self._temp_files[chunk_n] = temp_file
-        self._write_chunk_to_temp_file(self._temp_files[chunk_n], sorted_data)
+    def _sort_data(self, l_bound, r_bound, indent=1):
+        if r_bound - l_bound <= self._max_chunk_size:
+            sorted_data = self._sort_in_memory(l_bound, r_bound)
+            return self._save_to_temp_file(sorted_data)
+        else:
+            mid = l_bound + (r_bound - l_bound) // 2
+            if self._verbose:
+                print('{}[{}, {}]'.format('-' * indent, l_bound, mid))
+                print('{}[{}, {}]'.format('-' * indent, mid, r_bound))
+            temp_file_l = self._sort_data(l_bound, mid, indent + 1)
+            temp_file_r = self._sort_data(mid + 1, r_bound, indent + 1)
+            if self._verbose:
+                print('{} [{}, {}]'.format('+' * indent, l_bound, r_bound))
+            return self._merge(temp_file_l, temp_file_r)
 
-    def _read_chunk_from_init_file(self, path, chunk_n):
-        chunk_start, chunk_end = self._define_chunk_borders(chunk_n)
+    def _sort_in_memory(self, l_bound, r_bound):
+        data = self._read_chunk_from_init_file(l_bound, r_bound)
+        nd_array = np.array(data)
+        nd_array.sort()
+        return nd_array.astype(int)
+
+    def _read_chunk_from_init_file(self, chunk_start, chunk_end):
         chunk = []
-        with open(path, 'r') as fp:
-            try:
-                for l_number in range(chunk_start):
-                    next(fp)
-            except StopIteration:
-                raise InvalidChunkBordersError()
+        if chunk_end >= self._data_size:
+            chunk_end = self._data_size - 1
+        with open(self._input_path, 'r') as fp:
+            for l_number in range(chunk_start):
+                next(fp)
 
-            for l_number in range(chunk_end - chunk_start):
-                try:
-                    line = fp.readline()
-                    number = int(line)
-                    chunk.append(number)
-                except ValueError:
-                    raise InvalidDataError()
+            for l_number in range(chunk_end - chunk_start + 1):
+                line = fp.readline()
+                number = int(line)
+                chunk.append(number)
 
             return chunk
 
-    def _define_chunk_borders(self, chunk_n):
-        lower_bound = self._chunk_shifts[chunk_n]
-        upper_bound = self._chunk_shifts.get(chunk_n + 1, self._data_size)
-        return lower_bound, upper_bound
-
-    def _merge_sort(self, xs):
-        unit = 1
-        while unit <= len(xs):
-            h = 0
-            for h in range(0, len(xs), unit * 2):
-                l, r = h, min(len(xs), h + 2 * unit)
-                mid = h + unit
-                # merge xs[h:h + 2 * unit]
-                p, q = l, mid
-                while p < mid and q < r:
-                    if xs[p] < xs[q]:
-                        p += 1
-                    else:
-                        tmp = xs[q]
-                        xs[p + 1: q + 1] = xs[p:q]
-                        xs[p] = tmp
-                        p, mid, q = p + 1, mid + 1, q + 1
-
-            unit *= 2
-
-        return xs
-
-    def _create_temp_file(self):
-        return tempfile.TemporaryFile()
-
-    def _write_chunk_to_temp_file(self, temp_file, data):
+    def _save_to_temp_file(self, data):
+        temp_file = tempfile.TemporaryFile()
         for item in data:
             temp_file.write(bytes(str(item) + '\n', 'utf-8'))
         temp_file.seek(0)
+        return temp_file
 
-    def merge(self, result_path):
-        heap = [(self._try_read_next_item_from_temp_file(chunk_n)[1], chunk_n)
-                for chunk_n in range(self._chunk_count)]
+    def _merge(self, temp_file_1, temp_file_2):
+        temp_file = tempfile.TemporaryFile()
 
-        heapify(heap)
-        i = 0
-        with open(result_path, 'w+') as fp:
-            while any(self._temp_files.values()):
-                min_item, chunk_n = heappop(heap)
-                fp.write(str(min_item) + '\n')
-                success, next_item = self._try_read_next_item_from_temp_file(chunk_n)
-                if not success:
-                    self._temp_files[chunk_n].close()
-                    del self._temp_files[chunk_n]
-                    self._chunk_count -= 1
-                else:
-                    heappush(heap, (next_item, chunk_n))
+        success_1, num_1 = self._try_read_next_item_from_temp_file(temp_file_1)
+        success_2, num_2 = self._try_read_next_item_from_temp_file(temp_file_2)
 
-    def _try_read_next_item_from_temp_file(self, chunk_n):
-        file = self._temp_files[chunk_n]
+        alive_markers = [success_1, success_2]
+        while all(alive_markers):
+            if num_1 <= num_2:
+                temp_file.write(bytes(str(num_1) + '\n', 'utf-8'))
+                alive_markers[0], num_1 = self._try_read_next_item_from_temp_file(temp_file_1)
+            else:
+                temp_file.write(bytes(str(num_2) + '\n', 'utf-8'))
+                alive_markers[1], num_2 = self._try_read_next_item_from_temp_file(temp_file_2)
+
+        if not any(alive_markers):
+            temp_file.seek(0)
+            return temp_file
+
+        if alive_markers[0]:
+            alived = temp_file_1
+            temp_file.write(bytes(str(num_1) + '\n', 'utf-8'))
+        else:
+            alived = temp_file_2
+            temp_file.write(bytes(str(num_2) + '\n', 'utf-8'))
+
+        still_alive = True
+
+        while still_alive:
+            still_alive, num = self._try_read_next_item_from_temp_file(alived)
+            if still_alive:
+                temp_file.write(bytes(str(num) + '\n', 'utf-8'))
+
+        temp_file.seek(0)
+        return temp_file
+
+    def _try_read_next_item_from_temp_file(self, temp_file):
         number = None
         try:
-            line_of_bytes = file.readline()
+            line_of_bytes = temp_file.readline()
             int_str = line_of_bytes.decode('utf-8')
             number = int(int_str)
         except:
@@ -134,6 +117,15 @@ class ExternalMemoryMergeSort:
 
         return True, number
 
+    def _copy_from_temp(self, temp_file, output_path):
+        with open(output_path, 'w+') as fp:
+            for line in temp_file:
+                int_str = line.decode('utf-8')
+                if int_str == '':
+                    break
+                number = int(int_str)
+                fp.write(str(number) + '\n')
+
 
 if __name__ == '__main__':
-    ExternalMemoryMergeSort().sort('D:/sort/input.txt', 'D:/sort/output.txt')
+    ExternalMemoryMergeSort().sort('D:/sort/input.txt', 'D:/sort/output.txt', verbose=True)
