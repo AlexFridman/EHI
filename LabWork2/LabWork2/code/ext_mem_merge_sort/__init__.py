65a39a267559f696383e267fc0655e2b8d3674dd
Alex Fridman <AlexFridman@outlook.com>
2015-10-27 02:10:54 +0300

    Delete unused excpetions

--- LabWork2/LabWork2/code/ext_mem_merge_sort/__init__.py
+++ LabWork2/LabWork2/code/ext_mem_merge_sort/__init__.py
@@ -1,6 +1,4 @@
 __author__ = 'AlexF'
 
-from .invalid_chunk_borders_error import InvalidChunkBordersError
 from .invalid_data_error import InvalidDataError
-from .cannot_create_temp_file_error import CannotCreateTempFileError
 from .ext_mem_merge_sort import ExternalMemoryMergeSort
