d74dda7be2d2e3bccf85c6102bc68a8615bb1dd5
Alex Fridman <AlexFridman@outlook.com>
2015-12-07 21:31:40 +0300

    Add teacher index and detail views

--- LabWork3/Timetable/app/views.py
+++ LabWork3/Timetable/app/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from .models import Faculty
+from .models import Faculty, Teacher
 
 
 class FacultyIndexView(generic.ListView):
@@ -15,3 +15,17 @@ class FacultyDetailView(generic.DetailView):
     model = Faculty
     template_name = 'app/faculty/detail.html'
     context_object_name = 'faculty'
+
+
+class TeacherIndexView(generic.ListView):
+    template_name = 'app/teacher/index.html'
+    context_object_name = 'teacher_list'
+
+    def get_queryset(self):
+        return Teacher.objects.all()
+
+
+class TeacherDetailView(generic.DetailView):
+    model = Teacher
+    template_name = 'app/teacher/detail.html'
+    context_object_name = 'teacher'
