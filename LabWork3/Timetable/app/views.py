975acb11138c866fb11347a6a103e5f07ecd8c91
Alex Fridman <AlexFridman@outlook.com>
2015-12-07 22:00:25 +0300

    Add login requirement to index and detail views

--- LabWork3/Timetable/app/views.py
+++ LabWork3/Timetable/app/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views import generic
 
 from .models import Faculty, Teacher
@@ -10,11 +12,19 @@ class FacultyIndexView(generic.ListView):
     def get_queryset(self):
         return Faculty.objects.all()
 
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(FacultyIndexView, self).dispatch(request, *args, **kwargs)
+
 
 class FacultyDetailView(generic.DetailView):
     model = Faculty
     template_name = 'app/faculty/detail.html'
     context_object_name = 'faculty'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(FacultyDetailView, self).dispatch(request, *args, **kwargs)
 
 
 class TeacherIndexView(generic.ListView):
@@ -24,8 +34,16 @@ class TeacherIndexView(generic.ListView):
     def get_queryset(self):
         return Teacher.objects.all()
 
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(TeacherIndexView, self).dispatch(request, *args, **kwargs)
+
 
 class TeacherDetailView(generic.DetailView):
     model = Teacher
     template_name = 'app/teacher/detail.html'
     context_object_name = 'teacher'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(TeacherDetailView, self).dispatch(request, *args, **kwargs)
