336f37e664f27f86f80e5d8e649f6f06b3eb15e1
Alex Fridman <AlexFridman@outlook.com>
2015-12-07 10:04:43 +0300

    Fix. Add string representations to models

--- LabWork3/Timetable/app/models.py
+++ LabWork3/Timetable/app/models.py
@@ -5,6 +5,9 @@ class Place(models.Model):
     room = models.TextField(null=True)
     housing = models.IntegerField()
 
+    def __str__(self):
+        return '{}-{}'.format(self.room, self.housing)
+
 
 class Faculty(models.Model):
     class Meta:
@@ -12,15 +15,24 @@ class Faculty(models.Model):
 
     name = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Group(models.Model):
     number = models.IntegerField()
     faculty = models.ForeignKey(Faculty)
 
+    def __str__(self):
+        return str(self.number)
+
 
 class Teacher(models.Model):
     name = models.TextField()
     position = models.TextField()
+
+    def __str__(self):
+        return '{}, {}'.format(self.name, self.position)
 
 
 class Class(models.Model):
@@ -31,3 +43,7 @@ class Class(models.Model):
     place = models.ForeignKey(Place)
     teacher = models.ForeignKey(Teacher)
     group = models.ForeignKey(Group)
+    start_time = models.TimeField()
+
+    def __str__(self):
+        return '{}|{}|{}|{}|{}'.format(self.start_time, self.name, self.group, self.teacher, self.place)
