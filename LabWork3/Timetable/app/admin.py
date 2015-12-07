201ceee147c53bd9516d50780c704b0a3df51e5c
Alex Fridman <AlexFridman@outlook.com>
2015-12-07 09:50:42 +0300

    Add empty admin model and register them

--- LabWork3/Timetable/app/admin.py
+++ LabWork3/Timetable/app/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Faculty, Group, Class, Place, Teacher
+
+
+class FacultyAdmin(admin.ModelAdmin):
+    pass
+
+
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+
+class ClassAdmin(admin.ModelAdmin):
+    pass
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Faculty, FacultyAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Class, ClassAdmin)
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Teacher, TeacherAdmin)
