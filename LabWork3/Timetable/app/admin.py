03398088a3f79faaf21f7d7378d61adf76a2e62d
Alex Fridman <AlexFridman@outlook.com>
2015-12-07 10:16:31 +0300

    Add data. Set admin list, filter params

--- LabWork3/Timetable/app/admin.py
+++ LabWork3/Timetable/app/admin.py
@@ -4,23 +4,30 @@ from app.models import Faculty, Group, Class, Place, Teacher
 
 
 class FacultyAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name']
+    search_fields = ['name']
 
 
 class GroupAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['number', 'faculty']
+    list_filter = ['faculty']
+    search_fields = ['faculty']
 
 
 class ClassAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['start_time', 'name', 'group', 'teacher', 'place']
+    list_filter = ['start_time', 'name', 'group', 'teacher', 'place']
+    search_fields = ['name', 'group']
 
 
 class PlaceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['housing', 'room']
 
 
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'position']
+    list_filter = ['position']
+    search_fields = ['name']
 
 
 admin.site.register(Faculty, FacultyAdmin)
