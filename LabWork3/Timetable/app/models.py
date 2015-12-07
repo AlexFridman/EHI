9ab28f2065b9b3a5a65bd9c0593056a9d1646290
Alex Fridman <AlexFridman@outlook.com>
2015-12-07 09:46:26 +0300

    Add simple model

--- LabWork3/Timetable/app/models.py
+++ LabWork3/Timetable/app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    room = models.TextField(null=True)
+    housing = models.IntegerField()
+
+
+class Faculty(models.Model):
+    name = models.TextField()
+
+
+class Group(models.Model):
+    number = models.IntegerField()
+    faculty = models.ForeignKey(Faculty)
+
+
+class Teacher(models.Model):
+    name = models.TextField()
+    position = models.TextField()
+
+
+class Class(models.Model):
+    name = models.TextField()
+    place = models.ForeignKey(Place)
+    teacher = models.ForeignKey(Teacher)
+    group = models.ForeignKey(Group)
