1d8b274ed9914aaa55a1802a173056d17ed44c27
Alex Fridman <AlexFridman@outlook.com>
2015-12-07 12:02:06 +0300

    Add faculty index and detail views

--- LabWork3/Timetable/app/views.py
+++ LabWork3/Timetable/app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Faculty
+
+
+class FacultyIndexView(generic.ListView):
+    template_name = 'faculty/index.html'
+    context_object_name = 'faculty_list'
+
+    def get_queryset(self):
+        return Faculty.objects.all()
+
+
+class FacultyDetailView(generic.DetailView):
+    model = Faculty
+    template_name = 'faculty/detail.html'
+    context_object_name = 'faculty'
